1885a7197b4e105a35465175198538a422d1218d
RoberthCascante <robert.cascante.lopez@est.una.ac.cr>
2025-09-01 21:19:25 -0600

    eliminar configuración de Sentry del cliente

--- sentry.client.config.ts
+++
@@ -1,8 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-
-console.log('Sentry DSN:', process.env.NEXT_PUBLIC_SENTRY_DSN);
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || '',
-  tracesSampleRate: 1.0, // Ajusta este valor según tus necesidades
-  // Puedes agregar más opciones aquí
-});
